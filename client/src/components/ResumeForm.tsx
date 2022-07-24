a43dfda084fecc7cf30248993570a1b73baa7e46
dzekiczan1 <p.rzadkowolski@gmail.com>
2022-07-24 20:54:19 +0200

    installed tailwind.css added styles

--- client/src/components/ResumeForm.tsx
+++ client/src/components/ResumeForm.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import axios from "axios";
 import saveAs from "file-saver";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
 
 const ResumeForm = () => {
   const [name, setName] = useState({
@@ -28,40 +31,30 @@ const ResumeForm = () => {
 
     e.target.reset();
   };
+
   return (
-    <div>
-      <div>
-        <h3>Info</h3>
-        <hr />
-      </div>
+    <Paper elevation={3} className="m-8 p-6">
       <form onSubmit={nextStep}>
-        <div>
-          <div>
-            <label>Name</label>
-            <input
-              type="text"
-              name="name"
-              value={name.name}
-              onChange={(e) => setName({ ...name, name: e.target.value })}
-              required
-            />
-          </div>
-          <div>
-            <label>Surname</label>
-            <input
-              type="text"
-              name="surname"
-              value={name.surname}
-              onChange={(e) => setName({ ...name, surname: e.target.value })}
-              required
-            />
-          </div>
-        </div>
+        <TextField
+          required
+          id="outlined-required"
+          label="First Name"
+          defaultValue={name.name}
+          onChange={(e) => setName({ ...name, name: e.target.value })}
+        />
+
+        <TextField
+          required
+          id="outlined-required"
+          label="Last Name"
+          defaultValue={name.surname}
+          onChange={(e) => setName({ ...name, surname: e.target.value })}
+        />
         <div>
           <button type="submit">Download Resume</button>
         </div>
       </form>
-    </div>
+    </Paper>
   );
 };
 
