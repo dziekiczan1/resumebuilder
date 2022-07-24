3ec7b4b62218c993d77376b637d6a58a480f1f48
dzekiczan1 <p.rzadkowolski@gmail.com>
2022-07-24 19:08:36 +0200

    started server added routes for data

--- server/index.js
+++ server/index.js
@@ -13,4 +13,18 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.post("/resume-createpdf", (req, res) => {
+  pdf.create(resumeTemplate(req.body), {}).toFile("Resume.pdf", (err) => {
+    if (err) {
+      res.send(Promise.reject());
+      console.log(err);
+    }
+    res.send(Promise.resolve());
+  });
+});
+
+app.get("/resume-fetchpdf", (req, res) => {
+  res.sendFile(`${__dirname}/Resume.pdf`);
+});
+
 app.listen(port, () => console.log(`Server started on port ${port}`));
