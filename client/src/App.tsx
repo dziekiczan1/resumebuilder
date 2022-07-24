9d6d6c02bbb1efa5685aca362c80e756b3c7d904
dzekiczan1 <p.rzadkowolski@gmail.com>
2022-07-24 21:37:40 +0200

    added styles  to app component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,32 @@
 import React from "react";
 import "./App.css";
 import ResumeForm from "./components/ResumeForm";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
 
 const App = () => {
+  const steps = ["Personal Informations", "Work experiance", "Create an ad"];
+
   return (
-    <div>
-      <div>
-        <h1>Your Resume!</h1>
-        <p>Please fill in the form.</p>
-        <hr />
+    <>
+      <div className="mt-8 lg:text-center">
+        <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">
+          Resume builder
+        </h2>
+        <p className="mt-2 mb-12 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+          Let's create a resume for you!
+        </p>
+        <Stepper activeStep={0} alternativeLabel>
+          {steps.map((label) => (
+            <Step key={label}>
+              <StepLabel>{label}</StepLabel>
+            </Step>
+          ))}
+        </Stepper>
       </div>
       <ResumeForm />
-    </div>
+    </>
   );
 };
 
--- client/src/components/ResumeForm.tsx
+++ client/src/components/ResumeForm.tsx
@@ -9,6 +9,9 @@ const ResumeForm = () => {
   const [name, setName] = useState({
     name: "Name",
     surname: "Surname",
+    mobile: "Mobile number",
+    city: "City",
+    github: "Github profile",
   });
 
   const nextStep = (e: any) => {
@@ -34,6 +37,10 @@ const ResumeForm = () => {
 
   return (
     <Paper elevation={3} className="m-8 p-6">
+      <p className="mt-2 mb-12 text-lg leading-8 font-bold tracking-tight text-gray-900 sm:text-4xl">
+        Personal Informations
+      </p>
+
       <form onSubmit={nextStep}>
         <TextField
           required
@@ -49,6 +56,30 @@ const ResumeForm = () => {
           label="Last Name"
           defaultValue={name.surname}
           onChange={(e) => setName({ ...name, surname: e.target.value })}
+        />
+
+        <TextField
+          required
+          id="outlined-required"
+          label="Mobile"
+          defaultValue={name.mobile}
+          onChange={(e) => setName({ ...name, mobile: e.target.value })}
+        />
+
+        <TextField
+          required
+          id="outlined-required"
+          label="City"
+          defaultValue={name.city}
+          onChange={(e) => setName({ ...name, city: e.target.value })}
+        />
+
+        <TextField
+          required
+          id="outlined-required"
+          label="Github"
+          defaultValue={name.github}
+          onChange={(e) => setName({ ...name, github: e.target.value })}
         />
         <div>
           <button type="submit">Download Resume</button>
